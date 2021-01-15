acf3a097c93e31e90ef1e46499c04e7984fc8fb6
David Stinnett <davidstinnett@icloud.com>
2021-01-15 08:27:37 -0500

    seperated functionality into multiple files

---
+++ BookList-miniProject/src/books.js
@@ -0,0 +1,24 @@
+export const books = [
+	{
+		id: 1,
+		img:
+			"https://images-na.ssl-images-amazon.com/images/I/91uwocAMtSL._AC_UL200_SR200,200_.jpg",
+		title: "A Promised Land",
+		author: "Barack Obama",
+	},
+
+	{
+		id: 2,
+		img:
+			"https://images-na.ssl-images-amazon.com/images/I/71kxa1-0mfL._AC_UL200_SR200,200_.jpg",
+		title: "1984",
+		author: "George Orwell",
+	},
+	{
+		id: 3,
+		img:
+			"https://images-na.ssl-images-amazon.com/images/I/71wdbkiRokL._AC_UL200_SR200,200_.jpg",
+		title: "Animal Farm",
+		author: "George Orwell",
+	},
+];
--- BookList-miniProject/src/index.js
+++ BookList-miniProject/src/index.js
@@ -2,30 +2,8 @@ import React from "react";
 import ReactDom from "react-dom";
 import "./index.css";
 
-const books = [
-	{
-		id: 1,
-		img:
-			"https://images-na.ssl-images-amazon.com/images/I/91uwocAMtSL._AC_UL200_SR200,200_.jpg",
-		title: "A Promised Land",
-		author: "Barack Obama",
-	},
-
-	{
-		id: 2,
-		img:
-			"https://images-na.ssl-images-amazon.com/images/I/71kxa1-0mfL._AC_UL200_SR200,200_.jpg",
-		title: "1984",
-		author: "George Orwell",
-	},
-	{
-		id: 3,
-		img:
-			"https://images-na.ssl-images-amazon.com/images/I/71wdbkiRokL._AC_UL200_SR200,200_.jpg",
-		title: "Animal Farm",
-		author: "George Orwell",
-	},
-];
+import { books } from "./books";
+import Book from "./Book";
 
 function BookList() {
 	return (
@@ -36,17 +14,5 @@ function BookList() {
 		</section>
 	);
 }
-
-const Book = (props) => {
-	const { img, title, author } = props;
-	console.log(props);
-	return (
-		<article className="book">
-			<img src={img} alt={title + " cover"} />
-			<h1>{title}</h1>
-			<h4>{author}</h4>
-		</article>
-	);
-};
 
 ReactDom.render(<BookList />, document.getElementById("root"));
